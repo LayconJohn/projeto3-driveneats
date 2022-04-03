ac6965c5fe99460f821d9796e3d89eaa52c89579
Laycon <layconjohn.sg@gmail.com>
2022-04-02 21:39:48 -0300

    Opção de selecionar os itens (comida)

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
-function selecionarCard (elemento) {
-    const card = document.querySelector(".selecionar")
-    card.classList.toggle('selecionar')
-    
+function selecionarPrato (nomePrato) {
+    const prato = document.querySelector(`.${nomePrato} > .cardSelecionado`)
+    const cardClicado = document.querySelector(".oculto")
+    const estaClicado = cardClicado.classList.contains("oculto")
+    if (estaClicado === false) {
+        cardClicado.classList.add("oculto")
+    }
+
+    prato.classList.remove("oculto")
+}
+
+function selecionarBebida(nomeBebida) {
+    const card = document.querySelector(`.${nomeBebida} > .cardSelecionado`)
+    card.classList.remove("oculto")
 }
