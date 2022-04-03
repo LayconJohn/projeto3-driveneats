81a3b09ed2f3adc192836653ea25da8c17295992
Laycon <layconjohn.sg@gmail.com>
2022-04-02 21:50:25 -0300

    Adicionar opções de selecionar bebida e sobremesa

--- scripts.js
+++ scripts.js
@@ -1,15 +1,19 @@
 function selecionarPrato (nomePrato) {
-    const prato = document.querySelector(`.${nomePrato} > .cardSelecionado`)
+    const prato = document.querySelector(`.${nomePrato} .cardSelecionado`)
     const cardClicado = document.querySelector(".oculto")
     const estaClicado = cardClicado.classList.contains("oculto")
     if (estaClicado === false) {
         cardClicado.classList.add("oculto")
     }
-
     prato.classList.remove("oculto")
 }
 
 function selecionarBebida(nomeBebida) {
-    const card = document.querySelector(`.${nomeBebida} > .cardSelecionado`)
-    card.classList.remove("oculto")
+    const bebida = document.querySelector(`.${nomeBebida}  .cardSelecionado`)
+    bebida.classList.remove("oculto")
+}
+
+function selecionarSobremesa(nomeSobremesa) {
+    const sobremesa = document.querySelector(`.${nomeSobremesa} .cardSelecionado`)
+    sobremesa.classList.remove('oculto')
 }
