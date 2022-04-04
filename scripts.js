5c1a9bebb3002f5cf38c96a9ac2dc0d654204dd5
Laycon <layconjohn.sg@gmail.com>
2022-04-03 22:48:09 -0300

    Adicionar opção do botão fazer pedido

--- scripts.js
+++ scripts.js
@@ -1,19 +1,41 @@
+let pratoSelecionado;
+let bebidaSelecionada;
+let sobremesaSelecionada;
+
 function selecionarPrato (nomePrato) {
     const prato = document.querySelector(`.${nomePrato} .cardSelecionado`)
-    const cardClicado = document.querySelector(".oculto")
+    const cardClicado = document.querySelector(".pratos .oculto")
     const estaClicado = cardClicado.classList.contains("oculto")
     if (estaClicado === false) {
         cardClicado.classList.add("oculto")
     }
     prato.classList.remove("oculto")
+    pratoSelecionado = true;
+    console.log(pratoSelecionado)
 }
 
 function selecionarBebida(nomeBebida) {
     const bebida = document.querySelector(`.${nomeBebida}  .cardSelecionado`)
     bebida.classList.remove("oculto")
+    bebidaSelecionada = true;
+    console.log( bebidaSelecionada)
 }
 
 function selecionarSobremesa(nomeSobremesa) {
     const sobremesa = document.querySelector(`.${nomeSobremesa} .cardSelecionado`)
     sobremesa.classList.remove('oculto')
+    sobremesaSelecionada = true;
+    console.log(sobremesaSelecionada)
+} 
+
+function habilitarBotao(pratoSelecionado, bebidaSelecionada, sobremesaSelecionada) {
+    if (pratoSelecionado && bebidaSelecionada && sobremesaSelecionada) {
+        let botao = document.querySelector(".botao")
+        botao.classList.add("oculto")
+        let botaoEscondido = document.querySelector(".botao-escondido")
+        botaoEscondido.classList.remove("oculto")
+    } else {
+        alert('Olá')
+    }
 }
+
